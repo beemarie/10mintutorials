4d158c807e64369083a23570a415c99dc6d4b56a
Belinda Vennam <beemarie@us.ibm.com>
2020-11-24 11:16:54 -0500

    cleanup

--- cos-jobs/app/main.js
+++ cos-jobs/app/main.js
@@ -3,17 +3,19 @@ const stream = require('stream');
 const sharp = require('sharp');
 const path = require('path');
 
+if (!(process.env.APIKEY && process.env.ENDPOINT
+  && process.env.SERVICE_INSTANCE && process.env.BUCKET_NAME)) {
+  console.log('Missing required environment variables');
+}
 const apiKeyId = process.env.APIKEY;
 const endpoint = process.env.ENDPOINT;
 const serviceInstanceId = process.env.SERVICE_INSTANCE;
 const bucket = process.env.BUCKET_NAME;
-
 const config = {
   endpoint,
   apiKeyId,
   serviceInstanceId,
 };
-
 const cos = new objectStore.S3(config);
 
 function listObjects(thebucket) {
@@ -26,7 +28,6 @@ function uploadFromStream(thebucket, thekey) {
   const pass = new stream.PassThrough();
   const filename = path.parse(thekey).name;
   let extension = path.parse(thekey).ext;
-  console.log("UPLOADING ", filename, extension, thebucket, thekey);
   if (extension === '') {
     extension = '.png';
   }
@@ -43,10 +44,8 @@ async function main() {
   const objects = await listObjects(bucket);
   for (let i = 0; i < objects.Contents.length; i += 1) {
     const key = objects.Contents[i].Key;
-    console.log("KEY ", key);
     if (!key.includes('-thumbnail')) {
       const inStream = cos.getObject({ Bucket: bucket, Key: key }).createReadStream();
-      console.log("IN STREAM MADE");
       const transform = sharp().resize({ width: 200, height: 200 });
       inStream.pipe(transform).pipe(uploadFromStream(bucket, key));
     }
